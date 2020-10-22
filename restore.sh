106661b9fcf1ec1570e9e5cb2008f0bbd60310a7
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-10-23 01:44:38 +0900

    保存と反映をするスクリプト作成

---
+++ restore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mv ~/.spacemacs ~/.spacemacs.bak
+mv ~/.zshrc ~/.zshrc.bak
+cp ./.spacemacs ~/.spacemacs
+cp ./.zshrc ~/.zshrc
+
---
+++ save.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.spacemacs ./.spacemacs
+cp ~/.zshrc ./.zshrc
+
