db5dcbf59f872e9b09ed99c73d095b95dbe3587d
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-10-26 21:25:51 +0900

    dotfileを全部save/restoreするscript追加

---
+++ scripts/restore_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv ~/.zshrc ~/.zshrc.bak
+cp ../.zshrc ~/.zshrc
+mv ~/.spacemacs ~/.spacemacs.bak
+cp ../.spacemacs ~/.spacemacs
---
+++ scripts/save_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/.spacemacs ../.spacemacs
+cp ~/.zshrc ../.zshrc
